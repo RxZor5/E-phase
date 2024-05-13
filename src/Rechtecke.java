3904f0c8f9eaa6e5fe7e98da05dcb07de2a1838c
Julian <julian.bernauer26@gmail.com>
2024-05-13 12:51:23 +0200

    fi

--- src/Rechtecke.java
+++ src/Rechtecke.java
@@ -10,26 +10,29 @@ public class Rechtecke {
         int hoehe = sc.nextInt();
         System.out.println("Länge des Rechtecks: ");
         int laenge = sc.nextInt();
+        System.out.println("Einrückung: ");
+        int einrueckung = sc.nextInt();
 
-
-            zeichneRechteck(laenge, hoehe);
+            zeichneRechteck(laenge, hoehe, einrueckung);
 
 
     }
 
-    public static void zeichneRechteck(int laenge, int hoehe){
-
-        for (int i = 0; i <= hoehe; i++) {
-            System.out.print(" * ");
-
-            for (int j = 0; j <= laenge; j++) {
-                System.out.print(" * ");
+    public static void zeichneRechteck(int laenge, int hoehe, int einrueckung){
+        for (int i = 1; i <= hoehe; i++) {
+            if(einrueckung != 0) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("  ");
+                }
             }
-
+            for (int k = 0; k < laenge; k++) {
+                System.out.print(" *");
+            }
             System.out.println();
         }
+        }
 
-    }
+
 
 }
 
